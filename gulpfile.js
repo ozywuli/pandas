fe48f5c5b385e4b51ec680c9ee4d3758997275b7
Ozy Wu-Li <ozy.wu.li@gmail.com>
2016-07-02 17:21:31 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,155 @@
+/*
+ * Plugins
+ */
+
+ // General Plugins
+var gulp = require('gulp');
+var del = require('del');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var size = require('gulp-size');
+var notify = require("gulp-notify")
+
+// CSS Plugins
+var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var nanocss = require('gulp-cssnano');
+var mmq = require('gulp-merge-media-queries');
+
+// JS Plugins
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var gutil = require('gulp-util');
+
+// Image Plugins
+var imagemin = require('gulp-imagemin');
+
+/*
+ * Paths object
+ */
+var paths = {
+  srcAssets: 'src/assets/',
+  devAssets: 'dev/assets/',
+  buildAssets: 'build/assets/'
+}
+
+/*
+ * Handle errors
+ */
+function handleError(error) {
+  var message = error;
+  if (typeof error === 'function' ) { return; }
+  if (typeof error === 'object' && error.hasOwnProperty('message')) { message = error.message; }
+  if (message !== undefined) { console.log('Error: ' + message); }
+}
+
+gulp.task('clean', function() {
+  return del(['build']);
+});
+
+/*
+ * CSS
+ */
+gulp.task('css', function() {
+  gulp.src(paths.srcAssets + 'scss/main.scss')
+    .pipe(plumber({
+        errorHandler: function (err) {
+            console.log(err);
+            this.emit('end');
+        }
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dev + 'css/'))
+    .pipe(notify({message: 'CSS compiled', onLast: true}))
+});
+
+/*
+ * JS
+ */
+function compile(watch) {
+  var bundler = watchify(browserify(path.srcAssetes + 'js/main.js', {
+    debug: true,
+    extensions: ['js']
+  }).transform(babelify.configure({
+      presets: ["es2015"]
+    }))
+  );
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) {
+        console.error(err);
+        this.emit('end'); }
+      )
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest(path.devAssets + 'js'))
+      .pipe(notify({message: 'JS compiled', onLast: true}))
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+
+function watchJS() {
+  return compile(true);
+};
+
+
+gulp.task('js', function() {
+  return compile();
+});
+
+
+
+/*
+ * Images
+ */
+gulp.task('img', function() {
+  gulp.src(paths.srcAssets + 'img/**/*')
+    .pipe(gulp.dest(paths.buildAssets + 'img/'))
+    .pipe(notify({message: 'Images compiled!', onLast: true}))
+});
+
+/*
+ * Watch
+ */
+gulp.task('watch', function(error) {
+  gulp.watch(paths.srcAssets + 'scss/**/*', ['css']);
+  gulp.watch(paths.srcAssets + 'js/**/*', ['js']);
+  gulp.watch(paths.srcAssets + 'img/**/*', ['img']);
+
+  watchJS();
+});
+
+/*
+ * Build
+ */
+gulp.task('build', function() {
+  console.log('Ready to go!');
+});
+
+/*
+ * Default task -
+ */
+gulp.task('default', ['watch', 'css', 'js', 'img']);
